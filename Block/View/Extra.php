f60614c2f7cb235425ea3d7614adf042287eb5b0
magento1993 <magento1993@gmail.com>
2019-04-14 12:07:38 +0530

    Add files via upload

---
+++ Block/View/Extra.php
@@ -0,0 +1,9 @@
+<?php
+namespace Biren\ProductId\Block\View;
+
+use Magento\Catalog\Block\Product\AbstractProduct;
+
+class Extra extends AbstractProduct
+{
+
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Biren_ProductId',
+    __DIR__
+);
